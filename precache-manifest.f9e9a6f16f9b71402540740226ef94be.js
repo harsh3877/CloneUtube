865ee02acf99f22a0424d75639a335246ef7edd0
naga <na@gc.com>
2022-05-26 12:32:23 +0530

    Updates

---
+++ precache-manifest.f9e9a6f16f9b71402540740226ef94be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bffa2e5cf9df04912d8b",
+    "url": "/CloneUtube/static/css/main.af206cd3.chunk.css"
+  },
+  {
+    "revision": "bffa2e5cf9df04912d8b",
+    "url": "/CloneUtube/static/js/main.bffa2e5c.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/CloneUtube/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "271237c29629a4b94e28",
+    "url": "/CloneUtube/static/js/2.271237c2.chunk.js"
+  },
+  {
+    "revision": "47bc506d346fcd98a35c",
+    "url": "/CloneUtube/static/js/runtime~main.47bc506d.js"
+  },
+  {
+    "revision": "1c098a2127dcfa4db698fcaa6477150f",
+    "url": "/CloneUtube/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/CloneUtube/precache-manifest.f9e9a6f16f9b71402540740226ef94be.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/CloneUtube/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});

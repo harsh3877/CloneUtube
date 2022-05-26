200081cbd079cd2fbd1c569a6433eb87d9024c2e
naga <na@gc.com>
2022-05-26 12:40:20 +0530

    Updates

--- precache-manifest.29a9c458492502aac46ba1ef8955ab0d.js
+++ precache-manifest.29a9c458492502aac46ba1ef8955ab0d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "2eac03c71bdb80255f84",
+    "revision": "c80c87a44705a9b61272",
     "url": "/CloneUtube/static/css/main.af206cd3.chunk.css"
   },
   {
-    "revision": "2eac03c71bdb80255f84",
-    "url": "/CloneUtube/static/js/main.2eac03c7.chunk.js"
+    "revision": "c80c87a44705a9b61272",
+    "url": "/CloneUtube/static/js/main.c80c87a4.chunk.js"
   },
   {
     "revision": "af08dc77e4e76da41e54",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/CloneUtube/static/js/runtime~main.47bc506d.js"
   },
   {
-    "revision": "0aa948b881a20fae03a072b81bcdf9fe",
+    "revision": "77fb8680f037f56e7b041205bee000b7",
     "url": "/CloneUtube/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/CloneUtube/precache-manifest.3ed7f1bee407c33ce932156a45a3a366.js"
+  "/CloneUtube/precache-manifest.29a9c458492502aac46ba1ef8955ab0d.js"
 );
 
 workbox.clientsClaim();
